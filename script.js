5289b6715c417f6d304412ffab81e86216147056
nakulllllll <nakul33email.com@gmail.com>
2026-03-27 00:08:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const supabaseUrl = "https://pufvuhulkttkzkxrconb.supabase.co";
-const supabaseKey = "YOUR_KEY_HERE";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1ZnZ1aHVsa3R0a3preHJjb25iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0OTg1ODMsImV4cCI6MjA5MDA3NDU4M30.0l8UnwSKCfqe5NYo70TFcQqCC2o-BNBJga2zFS_CrG4";
 
 const client = Supabase.createClient(supabaseUrl, supabaseKey);
 
