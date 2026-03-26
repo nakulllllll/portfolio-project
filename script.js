38d21c14eced8f828c073d3f8d7ac49fa314c5b4
nakulllllll <nakul33email.com@gmail.com>
2026-03-27 00:08:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
-// Supabase
 const supabaseUrl = "https://pufvuhulkttkzkxrconb.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1ZnZ1aHVsa3R0a3preHJjb25iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0OTg1ODMsImV4cCI6MjA5MDA3NDU4M30.0l8UnwSKCfqe5NYo70TFcQqCC2o-BNBJga2zFS_CrG4";
+const supabaseKey = "YOUR_KEY_HERE";
+
 const client = Supabase.createClient(supabaseUrl, supabaseKey);
 
-// Toggle sections
-function toggleSection(element) {
-  const section = element.parentElement;
-
-  document.querySelectorAll("section").forEach(sec => {
-    if (sec !== section) {
-      sec.classList.remove("active");
-    }
-  });
-
-  section.classList.toggle("active");
-}
-
-// Form submit
-document.getElementById("form").addEventListener("submit", async function(e) {
+document.getElementById("form").addEventListener("submit", async function(e){
   e.preventDefault();
 
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const message = document.getElementById("message").value;
 
-  const { error } = await client.from("FEEDBACK").insert([
-    { NAME: name, EMAIL: email, MESSAGE: message }
-  ]);
+  const { error } = await client
+    .from("FEEDBACK")
+    .insert([{ NAME: name, EMAIL: email, MESSAGE: message }]);
 
-  if (error) {
+  if(error){
     document.getElementById("form-status").textContent = "Error sending message";
   } else {
     document.getElementById("form-status").textContent = "Message sent!";
